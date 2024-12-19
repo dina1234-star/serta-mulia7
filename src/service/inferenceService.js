896f712da95d72fd246ec6bd4c139fc79aa56b39
dina1234-star <c270b4kx1157@bangkit.academy>
2024-12-19 23:46:58 +0800

    inferenceService.js

--- src/service/inferenceService.js
+++ src/service/inferenceService.js
@@ -1,45 +1,42 @@
-const tf = require('@tensorflow/tfjs-node');
-const InputError = require('../exceptions/InputError');
- 
+const tf = require("@tensorflow/tfjs-node");
+const InputError = require("../exceptions/InputError");
+
 async function predictClassification(model, image) {
-    try {
-        const tensor = tf.node
-            .decodeJpeg(image)
-            .resizeNearestNeighbor([224, 224])
-            .expandDims()
-            .toFloat()
- 
-        const classes = ['Melanocytic nevus', 'Squamous cell carcinoma', 'Vascular lesion'];
- 
-        const prediction = model.predict(tensor);
-        const score = await prediction.data();
-        const confidenceScore = Math.max(...score) * 100;
- 
-        const classResult = tf.argMax(prediction, 1).dataSync()[0];
-        const label = classes[classResult];
- 
-        let explanation, suggestion;
- 
-        if(label === 'Melanocytic nevus') {
-            explanation = "Melanocytic nevus adalah kondisi di mana permukaan kulit memiliki bercak warna yang berasal dari sel-sel melanosit, yakni pembentukan warna kulit dan rambut."
-            suggestion = "Segera konsultasi dengan dokter terdekat jika ukuran semakin membesar dengan cepat, mudah luka atau berdarah."
-        }
- 
-        if(label === 'Squamous cell carcinoma') {
-            explanation = "Squamous cell carcinoma adalah jenis kanker kulit yang umum dijumpai. Penyakit ini sering tumbuh pada bagian-bagian tubuh yang sering terkena sinar UV."
-            suggestion = "Segera konsultasi dengan dokter terdekat untuk meminimalisasi penyebaran kanker."
-        }
- 
-        if(label === 'Vascular lesion') {
-            explanation = "Vascular lesion adalah penyakit yang dikategorikan sebagai kanker atau tumor di mana penyakit ini sering muncul pada bagian kepala dan leher."
-            suggestion = "Segera konsultasi dengan dokter terdekat untuk mengetahui detail terkait tingkat bahaya penyakit."
-        
-        }
- 
-        return { confidenceScore, label, explanation, suggestion };
-    } catch (error) {
-        throw new InputError(`Terjadi kesalahan input: ${error.message}`)
+  try {
+    const tensor = tf.node
+      .decodeImage(image)
+      .resizeNearestNeighbor([224, 224])
+      .expandDims()
+      .toFloat();
+
+    const prediction = model.predict(tensor);
+    const score = await prediction.data();
+    const confidenceScore = Math.max(...score) * 100;
+
+    console.log("score: ", score);
+    console.log("confidenceScore: ", confidenceScore);
+
+    // Model akan mengembalikan array dengan rentang nilai 0 hingga 1. Di mana jika rentang nilainya di atas 50% diklasifikasikan sebagai Cancer, jika di bawah atau sama dengan 50% diklasifikasikan sebagai Non-cancer.
+    const label = confidenceScore > 50 ? "Cancer" : "Non-cancer";
+
+    let suggestion;
+
+    if (label === "Cancer") {
+      suggestion =
+        "Segera periksa ke dokter!";
+    } else {
+      suggestion =
+        "Penyakit kanker tidak terdeteksi.";
     }
+
+    return {
+      confidenceScore,
+      label,
+      suggestion: suggestion,
+    };
+  } catch (error) {
+    throw new InputError(`Terjadi kesalahan dalam melakukan prediksi`);
+  }
 }
- 
+
 module.exports = predictClassification;
